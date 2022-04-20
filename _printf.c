bebd341e6b024519efc70caea581d5c9841cf209
Joseph Gitau <gitaujoseph64@gmail.com>
2022-04-20 15:01:19 +0300

    printf

---
+++ _printf.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Function for produces output according to a format
+ * @format: List of parameters to traverse
+ * Return: printf result
+ */
+
+int _printf(const char *format, ...)
+{
+	int i = 0, count = 0;
+	va_list valist;
+	int (*p)(va_list);
+
+	va_start(valist, format);
+
+	if (format == NULL)
+		return (-1);
+	if (format[0] == '%' && format[1] == '\0')
+		return (-1);
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			p = get_op_print(format + i);
+
+			if (p != NULL)
+			{
+				count += p(valist);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				count++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			count++;
+		}
+		i++;
+	}
+	va_end(valist);
+
+	return (count);
+}
